8abfe7de509799519ea0eeaa3768600b85f1e789
KinkySqueaky <subinasimulation@gmail.com>
2024-05-16 01:50:25 -0400

    Swapped to hashrouter for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Error from "./Error";
 import KinkList from "./components/KinkList/KinkList";
 import Home from "./components/Home/Home";
 import NavBar from "./components/NavBar";
 
 export default function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-      errorElement: <Error />,
-    },
-    {
-      path: "kinklist",
-      element: <KinkList />,
-    },
-  ]);
-
   return (
-    <BrowserRouter>
+    <HashRouter>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} errorElement={<Error />} />
+        <Route path="/" element={<Home />} />
         <Route path="/kinklist" element={<KinkList />} />
+        <Route path="*" element={<Error />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
